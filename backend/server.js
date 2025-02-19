6895df38705587e7f2e4901c681d3692c2f75834
Ralf_Victor <142431936+U-No-Who@users.noreply.github.com>
2025-02-19 23:21:03 +0530

    test server setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,34 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+const PORT = 8000;
+const CLIENT_URL = "http://localhost:3000";
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const server = async () => {
-  console.log("Server is Running...");
+  try {
+    app.listen(port, () => {
+      console.log(`Server is Running in Port ${port}`);
+    });
+  } catch (error) {
+    console.log("Failed to Start Server", error.message);
+    process.exit(1);
+  }
 };
 
 server();
