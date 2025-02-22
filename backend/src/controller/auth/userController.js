431e9bfda9799fb38846359c17b2cd4c884dfa16
Ralf_Victor <142431936+U-No-Who@users.noreply.github.com>
2025-02-22 09:42:29 +0530

    ft: User Login Status

--- backend/src/controller/auth/userController.js
+++ backend/src/controller/auth/userController.js
@@ -2,6 +2,7 @@ import asyncHandler from "express-async-handler";
 import User from "../../models/auth/UserModel.js";
 import generateToken from "../../helpers/generateToken.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -174,4 +175,16 @@ export const updateUser = asyncHandler(async (req, res) => {
   } else {
     res.status(404).json({ message: "User Not Found" });
   }
+});
+
+export const userLoginStatus = asyncHandler(async (req, res) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ message: "Not authorized. Please login" });
+  }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (decoded) {
+      res.status(200).json(true);
+    }
+    res.status(401).json(false);
 });
--- backend/src/routes/userroutes.js
+++ backend/src/routes/userroutes.js
@@ -5,8 +5,13 @@ import {
   logoutUser,
   registerUser,
   updateUser,
+  userLoginStatus,
 } from "../controller/auth/userController.js";
-import { adminMiddleware, creatorMiddleware, protect } from "../middleware/authMiddleware.js";
+import {
+  adminMiddleware,
+  creatorMiddleware,
+  protect,
+} from "../middleware/authMiddleware.js";
 import { deleteUser, getAllUsers } from "../controller/auth/adminController.js";
 
 const router = express.Router();
@@ -23,5 +28,8 @@ router.delete("/admin/users/:id", protect, adminMiddleware, deleteUser);
 
 //get all users
 router.get("/admin/users", protect, creatorMiddleware, getAllUsers);
+
+//login status
+router.get("/login-status", userLoginStatus);
 
 export default router;
